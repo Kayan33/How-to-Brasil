15925c13241b391155865dce48893d30ac488c9b
Kayan33 <kayanpereira33@gmail.com>
2023-11-23 12:11:14 -0300

    rodapé
    rodapé  em andamento, primeira parte feita (logo, links), agora falta a parte de baixo do rodapé

--- src/componentes/rodape/index.tsx
+++ src/componentes/rodape/index.tsx
@@ -4,29 +4,35 @@ import { Link } from "react-router-dom";
 function Rodape() {
   return (
     <div className="rodape-bg">
-      <a className="logo" href="/">
-        How to <br />
-        Brasil
-      </a>
-      <nav>
-        <ul className="rodape-link">
-          <li>
-            <a href="/">Inicio</a>
-          </li>
-          <li>
-            <a href="/">Inicio</a>
-          </li>
-          <li>
-            <a href="/">Inicio</a>
-          </li>
-          <li>
-            <a href="/">Inicio</a>
-          </li>
-        </ul>
-      </nav>
-      <Link className="saiba-mais" to="Cadastrar">
-          SAIBA MAIS
+      <div className="rodape-introducao">
+        <a className="logo" href="/">
+          How to <br />
+          Brasil
+        </a>
+        <nav>
+          <ul className="rodape-link">
+            <li>
+              <a href="/">Inicio</a>
+            </li>
+            <li>
+              <a href="/">Quem somos</a>
+            </li>
+            <li>
+              <a href="/">Recursos</a>
+            </li>
+            <li className="blog">
+              <a href="/">Blog</a>
+            </li>
+          </ul>
+        </nav>
+        <Link className="saiba-mais" to="Cadastrar">
+          Comece gratuitamente!
         </Link>
+
+      </div>
+        <div className="rodape-baixo">
+          <p>©2023 Tecnologia Ltda. CNPJ 00.000.000/0000-00</p>
+        </div>
     </div>
   );
 }
