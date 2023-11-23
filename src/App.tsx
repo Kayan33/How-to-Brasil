67542e8c2a60e716c2736aa8a57dee4d32864950
zardette <gabrielzardetto20@gmail.com>
2023-11-22 22:26:29 -0300

    Página de cadastro
    tsx e css em andamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Cadastrar from "./pages/Cadastrar";
 import Login from "./pages/Login";
 import Home from "./pages/home";
 import Saiba from "./pages/Saiba-mais";
 import Notfound from "./pages/notfound";
+import Cadastro from "./componentes/cadastrar/cadastrar";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <Route path='*' element={<Notfound/>}></Route>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/Login' element={<Login/>}></Route>
-        <Route path='/Cadastrar' element={<Cadastrar/>}></Route>
+        <Route path='/Cadastrar' element={<Cadastro/>}></Route>
         <Route path='/Saiba Mais' element={<Saiba/>}></Route>
         
       </Routes>
