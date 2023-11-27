46cb121a7085a4d1f17579f343e7a0e1a5b8b0ae
Kayan33 <kayanpereira33@gmail.com>
2023-11-27 14:36:03 -0300

    Slider finalizado

--- src/componentes/Slider/index.tsx
+++ src/componentes/Slider/index.tsx
@@ -1,10 +1,9 @@
 import { register } from "swiper/element/bundle";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFlip,EffectCoverflow, Pagination } from "swiper/modules";
+import { Navigation, EffectCoverflow, Pagination } from "swiper/modules";
 
-import "swiper/css/effect-coverflow";
 import "swiper/css";
-import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "../../style/style.css";
@@ -13,57 +12,92 @@ register();
 function Slider() {
   return (
     <>
-      <Swiper 
-        
+      <Swiper
+        effect={"coverflow"}
         navigation
-        
         loop={true}
         grabCursor={true}
         centeredSlides={true}
-        slidesPerView={1}
-        coverflowEffect={
-          {
-             rotate: 50,
-             stretch: 0,
-             depth: 100,
-             modifier: 1,
-             slideShadows: false,
-          }
-        }
-        pagination={{ clickable: true }}
+        slidesPerView={3}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        pagination={true}
         modules={[EffectCoverflow, Pagination]}
-        
-        
       >
+        <SwiperSlide>
+          <div>
+            <img src="/Slider/Slider1.png" />
+          </div>
+
+          <div className="slide-texto">
+            <h3>Saúde</h3>
+            <p>
+              Familiarize-se com o sistema de saúde local e comprove-se de ter
+              um plano de saúde adequado. Faça exames médicos regulares e
+              mantenha-se informado sobre os serviços médicos disponíveis em sua
+              área.
+            </p>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div>
+            <img src="/Slider/Slider2.png" />
+          </div>
+          <div className="slide-texto">
+            <h3>Buscar Emprego ou Educação</h3>
+            <p>
+              Se você ainda não tiver emprego, comece a procurar oportunidades
+              que se alinhem com suas habilidades e interesses.
+            </p>
+          </div>
+        </SwiperSlide>
         
-        <SwiperSlide className="flip">
-          <div className="front-card">
-          <img src="/Slider/Slider1.png" />
+        <SwiperSlide>
+          <div>
+            <img src="/Slider/Slider3.png" />
           </div>
-            
-          <div className="back-card">
-            <h1>Saúde</h1>
-            <p></p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="flip">
-          <div className="front-card">
-          <img src="/Slider/Slider2.png" />
-          </div>
-            
-          <div className="back-card">
-            <h1>Trabalho</h1>
-            <p></p>
+
+          <div className="slide-texto">
+          <h3>Busque Apoio Comunitário</h3>
+          <p>
+            Conecte-se com a comunidade local e outros imigrantes, seja através
+            de grupos online, eventos locais ou instituições de apoio a
+            imigrantes.
+          </p>
           </div>
         </SwiperSlide>
-        <SwiperSlide className="flip">
-          <div className="front-card">
-          <img src="/Slider/Slider3.png" />
+
+        <SwiperSlide>
+          <div>
+            <img src="/Slider/Slider2.png" />
           </div>
-            
-          <div className="back-card">
-            <h1>Apoio</h1>
-            <p></p>
+          <div className="slide-texto">
+            <h3>Buscar Emprego ou Educação</h3>
+            <p>
+              Se você ainda não tiver emprego, comece a procurar oportunidades
+              que se alinhem com suas habilidades e interesses.
+            </p>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div>
+            <img src="/Slider/Slider3.png" />
+          </div>
+
+          <div className="slide-texto">
+          <h3>Busque Apoio Comunitário</h3>
+          <p>
+            Conecte-se com a comunidade local e outros imigrantes, seja através
+            de grupos online, eventos locais ou instituições de apoio a
+            imigrantes.
+          </p>
           </div>
         </SwiperSlide>
       </Swiper>
