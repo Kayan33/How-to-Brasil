d231a451addec0935f7135113ce03016c410c9ec
Kayan33 <kayanpereira33@gmail.com>
2023-12-21 21:14:16 -0300

    [Refatoração] Atualização de Componente Perguntas e Estilo CSS
    - Componente Perguntas refatorado para utilizar TypeScript e interfaces.
    - Adicionada tipagem apropriada para a lista de perguntas e respostas.
    - Introdução de constantes e melhorias na lógica de expansão de perguntas.
    - Estilo CSS modularizado e organizado para o componente Perguntas.
    - Melhorias visuais, incluindo efeitos de hover e destaque para perguntas expandidas.
    
    Detalhes:
    - Introdução de uma interface PerguntaResposta para garantir tipagem adequada.
    - Adição de identificadores únicos (ID) para cada pergunta na lista.
    - Ajuste no estilo do componente para garantir uma experiência de usuário mais intuitiva.

--- src/componentes/perguntas/index.tsx
+++ src/componentes/perguntas/index.tsx
@@ -1,57 +1,79 @@
 import React, { useState } from "react";
 import "../../style/style.css";
 
-function Perguntas() {
+interface PerguntaResposta {
+  id: number;
+  pergunta: string;
+  resposta: string;
+}
+
+const TEMPO_DE_ESPERA = 150; 
+
+const Perguntas: React.FC = () => {
   const [perguntaExpandida, setPerguntaExpandida] = useState<number | null>(
     null
   );
 
-  const perguntasRespostas = [
+  const perguntasRespostas: PerguntaResposta[] = [
     {
+      id: 1,
       pergunta: "Preciso pagar alguma coisa?",
-      resposta: "Não, totalmente de graça."
+      resposta: "Não, totalmente de graça.",
     },
     {
+      id: 2,
       pergunta: "Outra pergunta?",
-      resposta: "Outra resposta."
+      resposta: "Outra resposta.",
     },
     {
+      id: 3,
       pergunta: "Outra pergunta?",
-      resposta: "Outra resposta."
+      resposta: "Outra resposta.",
     },
     {
+      id: 4,
       pergunta: "Outra pergunta?",
-      resposta: "Outra resposta."
+      resposta: "Outra resposta.",
     },
-    
   ];
+
+  const handlePerguntaClick = (index: number) => {
+    if (perguntaExpandida === index) {
+      
+      setPerguntaExpandida(null);
+    } else {
+      
+      setTimeout(() => {
+        setPerguntaExpandida(index);
+      }, TEMPO_DE_ESPERA);
+    }
+  };
 
   return (
     <div className="container-perguntas">
       <h1>Perguntas frequentes</h1>
       <ul className="container-peguntas-respostas">
-        {perguntasRespostas.map((item, index) => (
-          <li key={index}>
-            <div className="pergunta">
-              {item.pergunta}
-              <img
-                onClick={() =>
-                  setPerguntaExpandida(
-                    perguntaExpandida === index ? null : index
-                  )
-                }
-                src="mais.svg"
-              />
+        {perguntasRespostas.map(({ id, pergunta, resposta }, index) => (
+          <li
+            key={id}
+            className={`pergunta ${
+              perguntaExpandida === index ? "pergunta-expandida" : ""
+            }`}
+            onClick={() => handlePerguntaClick(index)}
+          >
+            <div>
+              {pergunta}
+              <img src="mais.svg" alt="Mais" />
             </div>
 
             {perguntaExpandida === index && (
-              <div className="resposta">{item.resposta}</div>
+              <div className="resposta">{resposta}</div>
             )}
           </li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default Perguntas;
