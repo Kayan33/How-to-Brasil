452d3d8c8fa3acae89bb5098ceb81f550a2f2344
Kayan Pereira <138801461+Kayan33@users.noreply.github.com>
2024-02-21 19:23:27 -0300

    refatoração do componente casa.

--- src/componentes/casa/index.tsx
+++ src/componentes/casa/index.tsx
@@ -6,102 +6,77 @@ import { api } from "../../api";
 
 const locaisDeTrabalho = [
   {
-    nome: "UBS Bela Vista",
-    endereco:
-      "R. Marçal de Arruda Campos, Quadra 4 - Centro, Bauru - SP, 17063-060",
-    telefone: "(14) 99683-9244",
-    horarioFuncionamento: {
-      segunda: "12:00 - 22:00",
-      terca: "12:00 - 22:00",
-      quarta: "12:00 - 22:00",
-      quinta: "12:00 - 22:00",
-      sexta: "12:00 - 22:00",
-      sabado: "12:00 - 16:00",
-      domingo: "12:00 - 16:00",
-    },
-    linkMapa: "https://www.google.com/",
+    nome: "Apartamento - Parque Bogota",
+    endereco: "Parque Bauru - Bauru",
+    area: "47m²",
+    quartos: 2,
+    valor: "R$ 600,00",
+    banheiro: 1,
+    foto: "https://i.im.ge/2024/02/22/g9vOdW.6677457054.jpg",
+    linkCorrelatos:
+      "https://www.imobiliariacentral.com/detalhes-imovel.php?cod_imovel=9942",
   },
   {
-    nome: "Farmacia",
-    endereco:
-      "R. Marçal de Arruda Campos, Quadra 4 - Centro, Bauru - SP, 17063-060",
-    telefone: "(14) 99683-9244",
-    horarioFuncionamento: {
-      segunda: "12:00 - 22:00",
-      terca: "12:00 - 22:00",
-      quarta: "12:00 - 22:00",
-      quinta: "12:00 - 22:00",
-      sexta: "12:00 - 22:00",
-      sabado: "12:00 - 16:00",
-      domingo: "12:00 - 16:00",
-    },
-    linkMapa: "https://www.google.com/",
+    nome: "Apartamento - Spazio Beluno",
+    endereco: "Parque Uniao - Bauru",
+    area: "47m²",
+    quartos: 2,
+    valor: "R$ 600,00",
+    banheiro: 1,
+    vagas:1,
+    foto: "https://i.im.ge/2024/02/22/g9vgTP.6687959233.jpg",
+    linkCorrelatos:
+      "https://www.imobiliariacentral.com/detalhes-imovel.php?cod_imovel=8691",
   },
   {
-    nome: "Posto de Saude",
-    endereco:
-      "R. Marçal de Arruda Campos, Quadra 4 - Centro, Bauru - SP, 17063-060",
-    telefone: "(14) 99683-9244",
-    horarioFuncionamento: {
-      segunda: "12:00 - 22:00",
-      terca: "12:00 - 22:00",
-      quarta: "12:00 - 22:00",
-      quinta: "12:00 - 22:00",
-      sexta: "12:00 - 22:00",
-      sabado: "12:00 - 16:00",
-      domingo: "12:00 - 16:00",
-    },
-    linkMapa: "https://www.google.com/",
+    nome: "Kitinete",
+    endereco: "Jardim Helena - Bauru",
+    area: "25m²",
+    quartos: 1,
+    valor: "R$ 600,00",
+    banheiro: 1,
+    vagas:1,
+    foto: "https://i.im.ge/2024/02/22/g9J1Bh.6687539123.jpg",
+    linkCorrelatos:
+      "https://www.imobiliariacentral.com/detalhes-imovel.php?cod_imovel=8691",
   },
   {
-    nome: "UBS Bela Vista",
-    endereco:
-      "R. Marçal de Arruda Campos, Quadra 4 - Centro, Bauru - SP, 17063-060",
-    telefone: "(14) 99683-9244",
-    horarioFuncionamento: {
-      segunda: "12:00 - 22:00",
-      terca: "12:00 - 22:00",
-      quarta: "12:00 - 22:00",
-      quinta: "12:00 - 22:00",
-      sexta: "12:00 - 22:00",
-      sabado: "12:00 - 16:00",
-      domingo: "12:00 - 16:00",
-    },
-    linkMapa: "https://www.google.com/",
+    nome: "Apartamento - Parque Bogota",
+    endereco: "Parque Bauru - Bauru",
+    area: "47m²",
+    quartos: 2,
+    valor: "R$ 600,00",
+    banheiro: 1,
+    vagas:1,
+    foto: "https://i.im.ge/2024/02/21/g9g9ur.Design-sem-nome.jpg",
+    linkCorrelatos:
+      "https://www.imobiliariacentral.com/detalhes-imovel.php?cod_imovel=8691",
   },
 
   {
-    nome: "UBS Bela Vista",
-    endereco:
-      "R. Marçal de Arruda Campos, Quadra 4 - Centro, Bauru - SP, 17063-060",
-    telefone: "(14) 99683-9244",
-    horarioFuncionamento: {
-      segunda: "12:00 - 22:00",
-      terca: "12:00 - 22:00",
-      quarta: "12:00 - 22:00",
-      quinta: "12:00 - 22:00",
-      sexta: "12:00 - 22:00",
-      sabado: "12:00 - 16:00",
-      domingo: "12:00 - 16:00",
-    },
-    linkMapa: "https://www.google.com/",
+    nome: "Apartamento - Parque Bogota",
+    endereco: "Parque Bauru - Bauru",
+    area: "47m²",
+    quartos: 2,
+    valor: "R$ 600,00",
+    banheiro: 1,
+    vagas:1,
+    foto: "https://i.im.ge/2024/02/21/g9g9ur.Design-sem-nome.jpg",
+    linkCorrelatos:
+      "https://www.imobiliariacentral.com/detalhes-imovel.php?cod_imovel=8691",
   },
 
   {
-    nome: "UBS Bela Vista",
-    endereco:
-      "R. Marçal de Arruda Campos, Quadra 4 - Centro, Bauru - SP, 17063-060",
-    telefone: "(14) 99683-9244",
-    horarioFuncionamento: {
-      segunda: "12:00 - 22:00",
-      terca: "12:00 - 22:00",
-      quarta: "12:00 - 22:00",
-      quinta: "12:00 - 22:00",
-      sexta: "12:00 - 22:00",
-      sabado: "12:00 - 16:00",
-      domingo: "12:00 - 16:00",
-    },
-    linkMapa: "https://www.google.com/",
+    nome: "Apartamento - Parque Bogota",
+    endereco: "Parque Bauru - Bauru",
+    area: "47m²",
+    quartos: "",
+    valor: "R$ 600,00",
+    banheiro: 1,
+    vagas:1,
+    foto: "https://i.im.ge/2024/02/21/g9g9ur.Design-sem-nome.jpg",
+    linkCorrelatos:
+      "https://www.imobiliariacentral.com/detalhes-imovel.php?cod_imovel=8691",
   },
 ];
 
@@ -130,53 +105,69 @@ function Trabalho() {
   );
   return (
     <div className="trabalho-container">
-    <header className="header-input">
-      <h1>TRABALHO</h1>
-      <div className="input-wrapper">
-      <input
-          type="text"
-          value={termoBusca}
-          onChange={(ev) => setTermoBusca(ev.target.value)}
-          placeholder="Digite o nome do local"
-        />
-      </div>
-    </header>
+      <header className="header-input">
+        <h1>TRABALHO</h1>
+        <div className="input-wrapper">
+          <input
+            type="text"
+            value={termoBusca}
+            onChange={(ev) => setTermoBusca(ev.target.value)}
+            placeholder="Digite o nome do local"
+          />
+        </div>
+      </header>
 
-    <main className="trabalho-bg">
+      <main className="trabalho-bg">
         {locaisFiltrados.map((local, index) => (
           <ul className="card" key={index}>
-            <h3>{local.nome}</h3>
+            <img src={local.foto} alt={local.nome} id="foto-casa" />
+
             <li className="header-card">
-              <h3>Endereço :</h3>
+              <h3>{local.nome}</h3>
+            </li>
+            <li className="header-card">
               <p>{local.endereco}</p>
             </li>
-            <li className="header-card">
-              <h3>Telefone :</h3>
-              <p>{local.telefone}</p>
-            </li>
-            <li className="header-card">
-              <h3>Horário de funcionamento :</h3>
-              <p>
-                {Object.entries(local.horarioFuncionamento).map(
-                  ([dia, horario]) => (
-                    <React.Fragment key={dia}>
-                      {`${dia}: ${horario}`}
-                      <br />
-                    </React.Fragment>
-                  )
-                )}
-              </p>
-            </li>
             <hr />
-            <a href={local.linkMapa}>
-              <img src="maps.svg" alt="" />
-            </a>
+            <li className="detalhes-imovel">
+              <li>
+                <h3>area</h3>
+                <p>{local.area}</p>
+              </li>
+
+              <li>
+                <h3>Quartos</h3>
+                <p>{local.quartos}</p>
+              </li>
+
+              <li>
+                <h3>Banheiro</h3>
+                <p>{local.banheiro}</p>
+              </li>
+              <li>
+                <h3>Vagas</h3>
+                <p>{local.vagas}</p>
+              </li>
+            </li>
+
+            <hr />
+            <ul className="detalhes-valores">
+              <li>
+                <h2>{local.valor}</h2>
+              </li>
+
+              <li>
+                {local.linkCorrelatos && (
+                  <a className="saiba-mais" href={local.linkCorrelatos} target="_blank">
+                  VER MAIS
+                </a>
+                )}
+              </li>
+            </ul>
           </ul>
         ))}
       </main>
-  </div>
-
-    
+    </div>
   );
 }
 
