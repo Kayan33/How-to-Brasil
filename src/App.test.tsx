ad1e65926d37089409e63c04349e06e93982b5d9
zardette <150203361+zardette@users.noreply.github.com>
2024-03-11 22:26:32 -0300

    teste de permissão e teste de interesse
    Testes da página logada (manter permissão) e teste dos campos interesses feito

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import{MemoryRouter} from  'react-router-dom'
+import '@testing-library/jest-dom/extend-expect'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe ('teste de integração 1', () => {
+
+  it('Testando permissão de acesso para a página de Usuário', () => {
+
+    render(
+            <MemoryRouter initialEntries={['/Usuario']}>  
+                <App />
+            </MemoryRouter>            
+        ); 
+
+        const ElementoNotFount = screen.getByText('Usuario sem permissão');
+        expect(ElementoNotFount).toBeInTheDocument();
+  })
 });
+
+it('Testando elemento interesses da página logada', () => {
+  const {container} = render (    
+    <MemoryRouter initialEntries={['/Usuario']}>
+      <App />
+    </MemoryRouter>
+  );
+
+  const usuarioInteresses = container.getElementsByClassName("Interesses");
+  expect (usuarioInteresses).toBeGreaterThan(0);
+})
+it('Testando número de elementos interesses da página logada', () => {
+  const {container} = render (    
+    <MemoryRouter initialEntries={['/Usuario']}>
+      <App />
+    </MemoryRouter>
+  );
+  const usuarioInteresses1 = container.getElementsByClassName("Interesses");
+  expect(usuarioInteresses1).toBe(1);
+
+  });
+
+export {};
